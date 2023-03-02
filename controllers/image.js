fbf87b520d95877fcc8815d2543c02e93173789e
saiyadm99 <niloyaali53@gmail.com>
2023-03-02 12:05:26 +0600

    api

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,22 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-	apiKey: '614c732ae5494ce7a6ae2d6036ea671b'
+	apiKey: '896f21e898d344baa14bfbcab7ecb2cc'
 });
 
 const handleApiCall = (req, res) => {
 	app.models
-		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-		.then(data => {
-			res.json(data);
-		})
-		.catch(err => res.status(400).json('unable to work with API'))
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
 
